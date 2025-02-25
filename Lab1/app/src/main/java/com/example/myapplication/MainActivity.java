8bbcdfd0800179d1711c68084bf1aae77a8af57b
Larisa Pantelimon <larisa.pantelimon@mta.ro>
2025-02-25 11:22:03 +0200

    Lab2

--- Lab1/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Lab1/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +11,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    public void buttonClick(View v)
+    {
+        TextView tv = (TextView)findViewById(R.id.textView);
+        tv.setText(getString(R.string.NewText));
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
