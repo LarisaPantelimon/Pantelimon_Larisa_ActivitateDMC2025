afeebeed4c6228c53ab4afef442989046b0b8acd
Larisa Pantelimon <larisa.pantelimon@mta.ro>
2025-02-25 11:48:51 +0200

    Lab3 extra

--- Lab1/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Lab1/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -11,11 +12,26 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    public void buttonClick(View v)
-    {
-        TextView tv = (TextView)findViewById(R.id.textView);
-        tv.setText(getString(R.string.NewText));
+    public void buttonClick(View v) {
+        EditText tv1 = (EditText) findViewById(R.id.editTextText2);
+        EditText tv2 = (EditText) findViewById(R.id.editTextText3);
+        TextView tv = (TextView) findViewById(R.id.textView2);
+
+        try {
+            String sTextFromET = tv1.getText().toString();
+            int nIntFromET = Integer.parseInt(sTextFromET);
+
+            String sTextFromET2 = tv2.getText().toString();
+            int nIntFromET2 = Integer.parseInt(sTextFromET2);
+
+            int suma = nIntFromET + nIntFromET2;
+
+            tv.setText(String.valueOf(suma));
+        } catch (NumberFormatException e) {
+            tv.setText();
+        }
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
